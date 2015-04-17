8a3baa6c53b6d4e941c96e2b0789563a14ae9a7b
paulb <paulb@ashridgetech.com>
2015-04-17 08:54:17 +0100

    Build: add more functions to dist file

--- buildDistJs.sh
+++ buildDistJs.sh
@@ -6,5 +6,7 @@ browserify -o dist/freesheet.js \
   -r ./target/coffeejs/parser/TextParser.js:text-parser \
   -r ./target/coffeejs/runtime/ReactiveRunner.js:reactive-runner \
   -r ./target/coffeejs/runtime/TextLoader.js:text-loader \
+  -r ./target/coffeejs/functions/CoreFunctions.js:core-functions \
   -r ./target/coffeejs/functions/PageFunctions.js:page-functions \
+  -r ./target/coffeejs/functions/TimeFunctions.js:time-functions \
   -r ./target/coffeejs/worksheet/TableWorksheet.js:table-worksheet
