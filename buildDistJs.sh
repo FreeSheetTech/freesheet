f9af2f2eeef7c97ebef28ef109a2f66a4a09e221
paulb <paulb@ashridgetech.com>
2015-09-15 13:00:27 +0100

    ReactiveFunctionRunner: update dist build

--- buildDistJs.sh
+++ buildDistJs.sh
@@ -9,7 +9,7 @@ node_modules/.bin/browserify -o $DIST_FILE \
   -r rx \
   -r lodash \
   -r ./target/coffeejs/parser/TextParser.js:text-parser \
-  -r ./target/coffeejs/runtime/ReactiveRunner.js:reactive-runner \
+  -r ./target/coffeejs/runtime/ReactiveFunctionRunner.js:reactive-runner \
   -r ./target/coffeejs/runtime/TextLoader.js:text-loader \
   -r ./target/coffeejs/functions/CoreFunctions.js:core-functions \
   -r ./target/coffeejs/functions/TimeFunctions.js:time-functions \
